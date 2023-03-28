ae9fff115cafc3e68ff3dd193812f83b30aa27a8
Andrey Ryakhovskiy <remryahirev@gmail.com>
2023-03-28 22:58:37 +0300

    engine names changed

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,17 +1,17 @@
 export const engineMap = {
-  // 'ChatGPT-4 32k': {
+  // 'GPT-4 32k': {
   //   model: 'gpt-4-32k',
   //   tokens: 32768,
   // },
-  'ChatGPT-4': {
+  'GPT-4': {
     model: 'gpt-4',
     tokens: 8192,
   },
-  'ChatGPT-3.5 Turbo': {
+  'GPT-3.5 Turbo': {
     model: 'gpt-3.5-turbo',
     tokens: 4096,
   },
-  'ChatGPT-3': {
+  'GPT-3': {
     model: 'text-davinci-003',
     tokens: 4097,
   },
@@ -20,4 +20,4 @@ export const engineMap = {
 export type EngineType = keyof typeof engineMap;
 
 export const ENGINES = Object.keys(engineMap);
-export const DEFAULT_ENGINE: EngineType = 'ChatGPT-4';
+export const DEFAULT_ENGINE: EngineType = 'GPT-4';
